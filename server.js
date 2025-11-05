df56a6c0e85cf41550ce2680b6da9fa4d3c4ee9c
Satvik Singh <satviksingh204@gmail.com>
2025-11-05 12:39:59 +0530

    Fixed server.js issues

--- server.js
+++ server.js
@@ -9,10 +9,15 @@ const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TO
 
 app.get('/call', async (req, res) => {
   try {
+
+
+    const messageText = " This is an SOS Call. Check the location sent to u via message";
+    const twimlUrl = `https://twimlets.com/message?Message%5B0%5D=${encodeURIComponent(messageText)}`;
+
     const call = await client.calls.create({
       from: process.env.TWILIO_FROM,
       to: process.env.TWILIO_TO,
-      url: 'This is an SOS Call. Check the location sent to u via message'
+      url: twimlUrl
     });
     res.send(`✅ Call initiated: ${call.sid}`);
   } catch (err) {
